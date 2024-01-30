4d5ab0059afd845c4974f83feef063a9de7749b7
Kawabe Kaito <chka21055@g.nihon-u.ac.jp>
2024-01-30 19:42:46 +0900

    使用方法書いた

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,25 @@
 import { Layout } from "antd";
 import HeaderPage from "./components/header";
+import KawarbeSpecial from "./components/kawarbeSpecial";
 
 const { Header, Content, Footer } = Layout;
 
 function App() {
-	return (
-		<Layout>
-			<Header>
-				<HeaderPage />
-			</Header>
-			<Content>
-				<h1>Content</h1>
-			</Content>
-			<Footer style={{ textAlign: "center" }}>
-				Syarin_no_hatsumei ©{new Date().getFullYear()} All Rights Reserved.
-			</Footer>
-		</Layout>
-	);
+  return (
+    <Layout>
+      <Header>
+        <HeaderPage />
+      </Header>
+
+      <Content>
+        <KawarbeSpecial />
+      </Content>
+
+      <Footer style={{ textAlign: "center" }}>
+        Syarin_no_hatsumei ©{new Date().getFullYear()} All Rights Reserved.
+      </Footer>
+    </Layout>
+  );
 }
 
 export default App;
