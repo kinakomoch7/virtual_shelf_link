26ec01688bf30aff8047dcfade6120a09be7b163
Kawabe Kaito <chka21055@g.nihon-u.ac.jp>
2024-01-31 01:30:23 +0900

    名前追加

--- src/App.jsx
+++ src/App.jsx
@@ -6,18 +6,27 @@ import { ProductLink } from "./components/ProductLink";
 const { Content, Footer } = Layout;
 
 function App() {
-	return (
-		<Layout>
-			<HeaderPage />
-			<Content>
+  return (
+    <Layout>
+      <HeaderPage />
+      <Content>
+        <div style={{ textAlign: "left", fontSize: "16px", margin: "10px" }}>
+          <div style={{ marginLeft: "10px" }}>チーム2 車輪の発明</div>
+
+          <div style={{ marginLeft: "10px", marginTop: "5px" }}>
+            小出朋希 川邉開人 石川翼 井上隆大
+          </div>
+        </div>
         <KawarbeSpecial />
-				<ProductLink />
-			</Content>
-			<Footer style={{ textAlign: "center", height:'20vh' , lineHeight: '13vh'}}>
-				Syarin_no_hatsumei ©{new Date().getFullYear()} All Rights Reserved.
-			</Footer>
-		</Layout>
-	);
+        <ProductLink />
+      </Content>
+      <Footer
+        style={{ textAlign: "center", height: "20vh", lineHeight: "13vh" }}
+      >
+        Syarin_no_hatsumei ©{new Date().getFullYear()} All Rights Reserved.
+      </Footer>
+    </Layout>
+  );
 }
 
 export default App;
