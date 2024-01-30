d416b9cdb5196e754400d04360d3dec7ff7e4881
Ishi <syi378039@gmail.com>
2024-01-30 18:02:14 +0900

    components作りました。

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,22 @@
-import { Layout } from "antd"
+import { Layout } from "antd";
+import HeaderPage from "./components/header";
 
 const { Header, Content, Footer } = Layout;
 
-
 function App() {
-
-  return (
-    <Layout>
-      <Header>
-        <h1>Header</h1>
-      </Header>
-      <Content>
-        <h1>Content</h1>
-      </Content>
-      <Footer style={{ textAlign: 'center' }}>
-        Syarin_no_hatsumei ©{new Date().getFullYear()} All Rights Reserved.
-      </Footer>
-    </Layout>
-  )
+	return (
+		<Layout>
+			<Header>
+				<HeaderPage />
+			</Header>
+			<Content>
+				<h1>Content</h1>
+			</Content>
+			<Footer style={{ textAlign: "center" }}>
+				Syarin_no_hatsumei ©{new Date().getFullYear()} All Rights Reserved.
+			</Footer>
+		</Layout>
+	);
 }
 
-export default App
+export default App;
---
+++ src/components/header.jsx
@@ -0,0 +1,9 @@
+const HeaderPage = () => {
+	return (
+		<>
+			<h1>Header</h1>
+		</>
+	);
+};
+
+export default HeaderPage;
